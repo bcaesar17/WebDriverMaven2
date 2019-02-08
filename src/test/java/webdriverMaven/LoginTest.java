764cf1b2d99c9367c30577075d0cb71dc33ca6ac
Bennie <test@t.com>
2019-02-07 19:01:42 -0500

    Initial Commit

---
+++ src/test/java/webdriverMaven/LoginTest.java
@@ -0,0 +1,39 @@
+package webdriverMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Owner\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		
+	}
+	
+	@Test
+	public void doLogin() throws InterruptedException {
+		
+		driver.get("http://www.way2automation.com/angularjs-protractor/banking/#/login");
+		Thread.sleep(4000);
+		driver.findElement(By.cssSelector("button[ng-click='manager()']")).click();
+		Thread.sleep(4000);
+		driver.findElement(By.cssSelector("button[ng-click='addCust()']")).click();
+				
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
